e3e0649de57fec289ed215ac2e7524a7201e5a34
zbc-123 <1946272954@qq.com>
2020-05-08 16:33:01 +0800

    haizei

---
+++ HZ167.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: HZ167.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 07 May 2020 11:39:12 AM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int main () {
+    char str[1005], buffer[1005];
+
+    return 0;
+}
